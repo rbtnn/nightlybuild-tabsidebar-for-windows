f40e8cb0335c31d437b22f53da35e90861b57960
Naruhiko Nishino <naru123456789@gmail.com>
2022-10-01 15:08:26 +0900

    Update vimver.bat

--- vimver.bat
+++ vimver.bat
@@ -9,5 +9,3 @@ if not exist version.h (
 set VIMSCRIPT=join(filter(map(readfile('version.h'),{_,s-^>matchstr(s,'^#define VIM_VERSION_\(MAJOR\^|MINOR\)\s\+\zs\d\+$')}),'!empty(v:val)') + filter(map(readfile('version.c'),{_,s-^>printf('%%04d',matchstr(s,'^\s\+\zs\d\+\ze,$'))}),'str2nr(v:val)')[:0], '.')
 set VIMSCRIPT=":call writefile(['::set-output name=vimver::' .. %VIMSCRIPT%], 'version.txt')"
 vim.exe -u NONO -N --cmd %VIMSCRIPT% --cmd ":qa!"
-type version.txt
-
