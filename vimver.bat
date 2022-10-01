4ad3dee39250b6b3d3c4a6b54a646df2eb6571ce
Naruhiko Nishino <naru123456789@gmail.com>
2022-10-01 15:18:35 +0900

    Update vimver.bat

--- vimver.bat
+++ vimver.bat
@@ -6,6 +6,6 @@ if not exist version.c (
 if not exist version.h (
 	curl -L https://raw.githubusercontent.com/vim/vim/master/src/version.h --output version.h
 )
-set VIMSCRIPT=join(filter(map(readfile('version.h'),{_,s-^>matchstr(s,'^#define VIM_VERSION_\(MAJOR\^|MINOR\)\s\+\zs\d\+$')}),'!empty(v:val)') + filter(map(readfile('version.c'),{_,s-^>printf('%%04d',matchstr(s,'^\s\+\zs\d\+\ze,$'))}),'str2nr(v:val)')[:0], '_')
+set VIMSCRIPT=join(filter(map(readfile('version.h'),{_,s-^>matchstr(s,'^#define VIM_VERSION_\(MAJOR\^|MINOR\)\s\+\zs\d\+$')}),'!empty(v:val)') + filter(map(readfile('version.c'),{_,s-^>printf('%%04d',matchstr(s,'^\s\+\zs\d\+\ze,$'))}),'str2nr(v:val)')[:0], '.')
 set VIMSCRIPT=":call writefile(['::set-output name=vimver::' .. %VIMSCRIPT%], 'version.txt')"
 vim.exe -u NONO -N --cmd %VIMSCRIPT% --cmd ":qa!"
